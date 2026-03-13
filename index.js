17217a38140b9102bb68afbf01804e256630cdbc
Emiliia <emiliakalenska@gmail.com>
2026-03-13 20:01:21 +0100

    JS: Hatered Development

--- index.js
+++ index.js
@@ -9,10 +9,37 @@ const form = document.getElementById("add-form");
 const cancelBtn = document.getElementById("cancel-btn");
 
 // Load from Local Storage
+function loadDictionary() {
+    const saved = localStorage.getItem("dictionary");
+    if (saved) dictionary = JSON.parse(saved) //json -> js object (because localStorage can only save strings)
+    renderTable();
+}
 
 // Save to Local Storage
+function saveDictionary() {
+    localStorage.setItem("dictionary", JSON.stringify) // js object -> json
+}
 
 // Render the Table
+function renderTable(){
+   tableBody.innerHTML = "";
+   if (dictionary.length === 0) {
+    tableBody.innerHTML = `
+    <tr>
+    <td colspan = "3" 
+    style="text-align:center; color:#666; padding:30px;>
+
+        No words yet. 
+        <br> Add some by tapping the <em>Add New</em> button. 
+
+     </td>
+    </tr>
+    `
+    return;
+   }
+}
+
+
 
 // Modal Controls
 

72f68ac036e00909268a1b4eb150316e2ad6cf21
Emiliia <emiliakalenska@gmail.com>
2026-03-13 17:42:54 +0100

    Step 3: JavaScript in the Project - Roadmap
    Tags assigned to Variables, Basic plan-out of what functions need to be rendered.

---
+++ index.js
@@ -0,0 +1,29 @@
+// Data
+let dictionary = [];
+
+// DOM Elements
+const tableBody = document.getElementById("table-body");
+const addBtn = document.getElementById("add-new-btn");
+const modal = document.getElementById("modal");
+const form = document.getElementById("add-form");
+const cancelBtn = document.getElementById("cancel-btn");
+
+// Load from Local Storage
+
+// Save to Local Storage
+
+// Render the Table
+
+// Modal Controls
+
+// Add New word
+
+// Cancel Button
+
+// Tap anywhere to close Modal
+
+// Delete
+
+// Open Modal
+
+// Start the App

1f9705eb978b3b511de5aa69396344c447cd39b9
Emiliia <emiliakalenska@gmail.com>
2026-03-14 13:51:35 +0100

    JS: Start Over - Data + Local Storage

--- index.js
+++ index.js
@@ -1,56 +1,15 @@
-// Data
+// словам треба ж десь бути і показуватись нет?
 let dictionary = [];
 
-// DOM Elements
-const tableBody = document.getElementById("table-body");
-const addBtn = document.getElementById("add-new-btn");
-const modal = document.getElementById("modal");
-const form = document.getElementById("add-form");
-const cancelBtn = document.getElementById("cancel-btn");
-
-// Load from Local Storage
-function loadDictionary() {
-    const saved = localStorage.getItem("dictionary");
-    if (saved) dictionary = JSON.parse(saved) //json -> js object (because localStorage can only save strings)
-    renderTable();
+// закачати слова в сєйф на браузєрі, бо десь ж мають лежати поки ти не вчишся 
+function saveToStorage() {
+  localStorage.setItem("myDictionary", JSON.stringify(dictionary));
 }
 
-// Save to Local Storage
-function saveDictionary() {
-    localStorage.setItem("dictionary", JSON.stringify) // js object -> json
+// викачати слова з сєйфа, але пропусти через зміну формату
+function loadFromStorage(){
+    const savedData = localStorage.getItem("myDictionary")
+    if (savedData) {
+        dictionary = JSON.parse(savedData)
+    }
 }
-
-// Render the Table
-function renderTable(){
-   tableBody.innerHTML = "";
-   if (dictionary.length === 0) {
-    tableBody.innerHTML = `
-    <tr>
-    <td colspan = "3" 
-    style="text-align:center; color:#666; padding:30px;>
-
-        No words yet. 
-        <br> Add some by tapping the <em>Add New</em> button. 
-
-     </td>
-    </tr>
-    `
-    return;
-   }
-}
-
-
-
-// Modal Controls
-
-// Add New word
-
-// Cancel Button
-
-// Tap anywhere to close Modal
-
-// Delete
-
-// Open Modal
-
-// Start the App

cc88ac433f1fb1bb1bc81e261e1c69f67435d12b
emikatop <emiliakalenska@gmail.com>
2026-07-11 15:21:05 +0200

    POST, DELETE, PATCH routes added

--- server.js
+++ server.js
@@ -3,13 +3,36 @@ const cors = require('cors');
 const db = require('./database.js');
 const app = express();
 
-//Middleware
+// --- Middleware
 app.use(cors()); // дозволяє фронтенду звертатись до сервера
 app.use(express.json()); //allows the server to parse(converts json->js object)incoming JSON data in the request body.
 
+// --- GET route to fetch all words from the database
 app.get('/words', (req, res) => {
     const words = db.prepare('SELECT * FROM words').all();
     res.json(words);
+})
+
+// --- POST route to add a new word to the database
+app.post('/words', (req, res) => {
+    const { word, example, translation} = req.body;
+    const result = db.prepare('INSERT INTO words (word, example, translation) VALUES (?, ?, ?)').run(word, example, translation);
+    res.json({ id: result.lastInsertRowid, word, example, translation});
+})
+
+// --- DELETE route to remove a word from the database
+app.delete('/words/:id', (req, res) => {
+    const {id} = Number(req.params.id);
+    db.prepare('DELETE FROM words WHERE id = ?').run(id);
+    res.json({message: 'Word deleted'});
+})
+
+// --- PUT route to edit a word in the database
+app.put('/words/:id', (req, res) => {
+    const {id} = Number(req.params.id);
+    const { word, example, translation } = req.body;
+    db.prepare('UPDATE words SET word = ?, example = ?, translation = ? WHERE id = ?').run(word, example, translation, id);
+    res.json({message: 'Wordupdated'});
 })
 
 //Listen to port 3000
